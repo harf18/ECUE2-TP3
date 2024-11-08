2276ef88d04688b1de0ef3b9543d74fbd54cceb7
Geoffroy Vibrac <geoffroy.vibrac@gmail.com>
2024-11-08 17:45:23 +0100

    initial commit

---
+++ src/net/lecnam/ecue2/tp3/Exec.java
@@ -0,0 +1,12 @@
+package net.lecnam.ecue2.tp3;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Exec  {
+
+    public static void main(String[] args) {
+
+    }
+
+}
